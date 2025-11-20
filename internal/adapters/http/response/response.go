5af407a6a2ac144406826fdde73f62f24482792a
Edward(Mac) <lankovedik@gmail.com>
2025-11-20 14:47:21 +0300

    refactor: router, and config , add handler and add standart response

---
+++ internal/adapters/http/response/response.go
@@ -0,0 +1,23 @@
+package response
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+// JSON отправляет JSON response
+func JSON(w http.ResponseWriter, statusCode int, data interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+
+	if err := json.NewEncoder(w).Encode(data); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+// Error отправляет JSON error response
+func Error(w http.ResponseWriter, statusCode int, message string) {
+	JSON(w, statusCode, map[string]string{
+		"error": message,
+	})
+}
